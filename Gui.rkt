c6a1849906f148442c186d0be54fc12693c650ce
rvenegas <risaacvene24@gmail.com>
2020-10-08 01:38:29 -0600

    Se agrega tabla-panel
    Se reemplaza la estructura básica por una tabla-panel y se da formato. Se agregan más imágenes y de diferentes tamaños. Se utiliza canvas

--- Gui.rkt
+++ Gui.rkt
@@ -1,31 +1,51 @@
- #lang racket/gui
+#lang racket/gui
 
 (require racket/gui/base)
+(require table-panel)
 
 (define frame (new frame%
-                 [label "Chinese checkers"]
-                [height 700]
-                [width 1400]))
+                 [label "Damas Chinas"]
+                 ;[stretchable-width #f] [stretchable-height #f]
+                 [height 700][width 700]
+                ))
+(define pos 1)
 
-; Add a horizontal panel to the dialog, with centering for buttons
-(define panel (new horizontal-panel% [parent frame]
-                                     [alignment '(center center)]))
+(define table-panel
+  (instantiate table-panel%
+    (frame)
+    (style '(border))
+    (alignment '(center center))
+    (dimensions '(13 13))))
 
-(define vertical-panel (new vertical-panel% [parent frame]
-                                     [alignment '(center center)]))
-
-(define list-of-panels '(panel panel))
+(for ((i (in-range 169)))
+  (let ((child (instantiate table-panel%
+                 (table-panel)
+                 (style '(border))
+                 (dimensions '(1 1))
+                 )))
+    (instantiate button%
+        ((read-bitmap "fotos/blue.png") child))
+    ))
 
 
 (define button-list
-  ' (("fotos/black.png" "fotos/blue.png" "fotos/red.png")
-     ("fotos/black.png" "fotos/blue.png" "fotos/red.png")))
+  '("fotos/black.png" "fotos/blue.png" "fotos/red.png" "fotos/wood.png"))
 
 
-(for ([i button-list])
-     (for ([j i])
-       (new button% [parent panel]
-       [label (read-bitmap j)])))
+
+
+(define bm (read-bitmap "fotos/base.jpg"))
+
+(define mycanvas%
+  (class canvas%
+    (super-new)
+    (inherit get-dc)
+    (define/override (on-paint)
+      (let ([my-dc (get-dc)])
+        (send my-dc draw-bitmap bm pos pos)))))
+
+(define c (new mycanvas% [parent frame] ))
+
 
 
 
@@ -33,3 +53,11 @@
 (send frame show #t)
 
 
+;(for ([i button-list])
+;     (for ([j i])
+;       (cond
+;         [(equal? (index-of button-list i) 0) (new button% [parent vertical-panel]
+;                        [label (read-bitmap j)])]
+;         [else (new button% [parent panel]
+;                    [label (read-bitmap j)])]
+;         )))
