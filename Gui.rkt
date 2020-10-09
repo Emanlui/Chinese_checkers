022e85432ca27c87b421474d18a3c17cbe1f48d7
Emanuelle <emanuellejs1999@gmail.com>
2020-10-08 18:08:50 -0600

    Se realiza la función de los click, almacena la posición y si se clickeo

--- Gui.rkt
+++ Gui.rkt
@@ -16,6 +16,10 @@
     (alignment '(center center))
     (dimensions '(13 13))))
 
+(define first-click #f)
+(define second-click #f)
+(define first-position 0)
+(define second-position 0)
 
 (define wood "fotos/wood.png")
 (define black "fotos/black.png")
@@ -41,34 +45,20 @@
 
 
 (for ((i (in-range 169)))
-  (let ((child (instantiate table-panel%
-                 (table-panel)
-                 (style '(border))
-                 (dimensions '(1 1))
-                 )))
+  (let ((child (new panel%
+             [parent table-panel]
+             [style '(border)]
+             [alignment '(center center)]
+             )))
     (instantiate button%
-        ((read-bitmap (list-ref list-of-image i)) child))
+        ((read-bitmap (list-ref list-of-image i) )
+         child)  [callback (lambda (button event)
+                         (button-click i))])
     ))
 
-(define mycanvas%
-  (class canvas%
-    (super-new)
-    (inherit get-dc)
-    (define/override (on-paint)
-      (let ([my-dc (get-dc)])
-        (send my-dc draw-bitmap bm 1 1)))))
-
-(define c (new mycanvas% [parent frame] ))
-
+(define (button-click i)
+    (cond
+     [(false? first-click) (set! first-click #t) (set! second-click #f) (set! first-position i) (set! second-position 0)]
+     [else (set! first-click #f)(set! second-click #t)(set! second-position i) (set! first-position 0)] ))
 
 (send frame show #t)
-
-
-;(for ([i button-list])
-;     (for ([j i])
-;       (cond
-;         [(equal? (index-of button-list i) 0) (new button% [parent vertical-panel]
-;                        [label (read-bitmap j)])]
-;         [else (new button% [parent panel]
-;                    [label (read-bitmap j)])]
-;         )))
