e894e1f070c20a7686c6999c17fa6d6a08c94b73
Emanuelle <emanuellejs1999@gmail.com>
2020-10-09 20:43:47 -0600

    El juego ya puede ganar, deshabilita botones y muestra un mensaje

--- Gui.rkt
+++ Gui.rkt
@@ -9,7 +9,7 @@
                  [height 700][width 700]
                 ))
 
-(define table-panel
+(define board
   (instantiate table-panel%
     (frame)
     (style '(border))
@@ -39,14 +39,14 @@
         red red red black black   black black black black
         red red red red black  black black black black))
 
-(define create-table
+(define grid-board
 (for ((i (in-range 81)))
   (let ((child (new panel%
-             [parent table-panel]
+             [parent board]
              [style '(border)]
              [alignment '(center center)]
              )))
-    (new button% [parent (list-ref (send table-panel get-children) i)]
+    (new button% [parent (list-ref (send board get-children) i)]
              [label (read-bitmap (list-ref list-of-image i))]
              [callback (lambda (button event) (button-click i (list-ref list-of-image i)))])
     )))
@@ -56,18 +56,48 @@
      [(false? first-click) (set! first-click #t) (set! second-click #f) (set! first-position i) (set! second-position 0)]
      [else (set! first-click #f)(set! second-click #t)(set! second-position i) (change-color) (set! first-position 0)] ))
 
+; Change the color on the matrix
 (define (change-color)
+  (cond [(not(equal? first-position second-position))
+  ; Sets the temp color of the buttons on variables
   (set! first-tmp-color (list-ref list-of-image first-position)) (set! second-tmp-color (list-ref list-of-image second-position))
-  (list-set list-of-image first-position second-tmp-color)(list-set list-of-image second-position first-tmp-color)
+  ; Sets the data on the matrix depending on the position
+  (set! list-of-image(list-set list-of-image first-position second-tmp-color))(set! list-of-image (list-set list-of-image second-position first-tmp-color))
+  ; Deletes the button
   (delete-button first-position) (delete-button second-position)
-  (add-button first-position)  (add-button second-position))
+  ; Add new buttons
+  (add-button first-position)  (add-button second-position)
+  (winner)]))
 
+; Delete a button on the board
 (define (delete-button index)
-  (send (list-ref (send table-panel get-children) index)
-        delete-child (list-ref (send (list-ref (send table-panel get-children) index) get-children) 0)))
+  ;      This is the board panel in this exact position
+  (send (list-ref (send board get-children) index)
+        ; This is the delete option and the child, [board [panel position [first object or the button, there is only one]]]
+        delete-child (list-ref (send (list-ref (send board get-children) index) get-children) 0)))
 
+; Add a button in a position on the board
 (define (add-button index)
-  (send (list-ref (send table-panel get-children) index) after-new-child (new button% [parent (list-ref (send table-panel get-children) index)]
+  ;      This gets the panel object on the grid    create a new child     And this is the new child
+  (send (list-ref (send board get-children) index) after-new-child (new button% [parent (list-ref (send board get-children) index)]
              [label (read-bitmap (list-ref list-of-image index))]
              [callback (lambda (button event) (button-click index (list-ref list-of-image index)))])))
+
+(define (winner)
+  (cond [(not(false? (blue-winner))) (disable-buttons) (define dialog (new dialog% [label "Winner winner chicken dinner"][height 200][width 200]))
+  (define msg (new message% [parent dialog]
+                          [label "Blue player wins"]))(send dialog show #t)  ]))
+
+(define (disable-buttons)
+  (for ([j (send board get-children)]) ; iterator binding
+  (send (list-ref (send j get-children) 0) enable #f)))
+
+; This function verifies if the blue side won
+(define (blue-winner)
+  (cond[
+        (not(false? ( and (equal? (list-ref list-of-image 50) blue)))) #t]
+       [else #f]))
+; This function verifies if the red side won
+
+; Show the frame to the screen
 (send frame show #t)
